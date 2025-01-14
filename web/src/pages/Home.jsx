5d5e2cc44c083062a0df4157f0dd0290ef66316a
karimi <behrouzkarimi1993@gmail.com>
2025-01-14 13:24:37 +0000

    login button added

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import Login from "../components/Login";
+
 function App() {
 	return (
 		<>
+			<Login />
 			<h1>Welcome to the Slack Dasjboard project.</h1>
 			<h2>Under development...</h2>
 		</>
