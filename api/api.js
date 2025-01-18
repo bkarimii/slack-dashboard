ad6838017d0e6f9c9bcaa6729702c192d199070c
karimi <behrouzkarimi1993@gmail.com>
2025-01-18 00:44:58 +0000

    endpoint refined. redundant code removed

--- api/api.js
+++ api/api.js
@@ -1,8 +1,5 @@
-/* eslint-disable no-console */
-
 import { Router } from "express";
 
-import { getMessagesFromChannel } from "./functions/getMessagesfromChannel.js";
 import { searchForUsersMessages } from "./functions/searchUsersMessages.js";
 import messageRouter from "./messages/messageRouter.js";
 
@@ -12,15 +9,11 @@ api.use("/message", messageRouter);
 
 api.get("/user-messages", async (req, res) => {
 	const { userId, channelId } = req.body;
-	console.log(userId, "==============");
 
 	try {
 		const messagesAndUsers = await searchForUsersMessages(userId, channelId);
-		console.log(messagesAndUsers, "<---------------Variable");
-		console.log(getMessagesFromChannel(userId), "<---------FUNCTION");
 		res.status(200).json(messagesAndUsers);
 	} catch (err) {
-		console.error(err.message);
 		res.status(500).json({ message: err });
 	}
 });
