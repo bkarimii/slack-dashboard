7a06bd414b75f10e64e99c132dbd461c0217ea3b
karimi <behrouzkarimi1993@gmail.com>
2025-01-21 00:48:20 +0000

    Users activity component added to the home component

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Login from "../components/Login";
+import UsersActivity from "../components/UsersActivity";
 
 function App() {
 	return (
 		<>
 			<Login />
-			<h1>Welcome to the Slack Dasjboard project.</h1>
+			<h1>Welcome to the Slack Dashboard project.</h1>
 			<h2>Under development...</h2>
+			<UsersActivity />
 		</>
 	);
 }
