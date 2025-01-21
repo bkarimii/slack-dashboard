b9164d4a40a64cef65c6bd972c3287d1e6bdf3c0
karimi <behrouzkarimi1993@gmail.com>
2025-01-21 00:45:47 +0000

    an endpoint to connect Fe and BE to display users activity

--- api/api.js
+++ api/api.js
@@ -1,9 +1,25 @@
 import { Router } from "express";
 
+import { userFinderFunction } from "./DummyData/userFinderFunction.js";
 import messageRouter from "./messages/messageRouter.js";
 
 const api = Router();
 
 api.use("/message", messageRouter);
+
+api.post("/user-activity", async (req, res) => {
+	const { userId } = req.body;
+
+	try {
+		const foundUser = userFinderFunction(userId);
+		if (foundUser.success) {
+			res.status(200).json(foundUser);
+		} else {
+			res.status(404).json({ message: "user not found" });
+		}
+	} catch (error) {
+		res.status(500).json({ err: error });
+	}
+});
 
 export default api;
