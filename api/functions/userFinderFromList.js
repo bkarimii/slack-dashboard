c18937a880327ac28c71ac639f726b2b78587c7a
karimi <behrouzkarimi1993@gmail.com>
2025-01-24 15:47:38 +0000

    userFinder search for users in a static list - avoid sending unnecessary api requests

---
+++ api/functions/userFinderFromList.js
@@ -0,0 +1,10 @@
+import { USERS } from "../../constants/USERS.js";
+
+export function userFinderFromList(userId) {
+	const foundUser = USERS.find((user) => user.userId === userId);
+	if (foundUser) {
+		return { success: true, user: foundUser };
+	} else {
+		return { success: false, user: "user not found" };
+	}
+}
