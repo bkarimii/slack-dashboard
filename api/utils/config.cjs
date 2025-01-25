50462efba4a88ca1ff78de429d85e8aeaac2db2e
karimi <behrouzkarimi1993@gmail.com>
2025-01-25 14:51:08 +0000

    config updated to handle webClient class from salck api package and is being exported to be used in other modules

--- api/utils/config.cjs
+++ api/utils/config.cjs
@@ -1,6 +1,8 @@
+/* eslint-disable n/no-extraneous-require */
 /* eslint-disable no-restricted-syntax */
 const { join, resolve } = require("node:path");
 
+const { WebClient } = require("@slack/web-api");
 const { configDotenv } = require("dotenv");
 
 const dotenvPath = resolve(
@@ -9,9 +11,11 @@ const dotenvPath = resolve(
 
 configDotenv({ path: dotenvPath });
 
-requireArgs(["DATABASE_URL"]);
+requireArgs(["DATABASE_URL", "SLACK_TOKEN"]);
 
 const databaseUrl = new URL(process.env.DATABASE_URL);
+const web = new WebClient(process.env.SLACK_TOKEN);
+const slackToken = process.env.SLACK_TOKEN;
 
 const localDb = [
 	"0.0.0.0",
@@ -31,6 +35,8 @@ const sslMode = ["prefer", "require", "verify-ca", "verify-full"].includes(
  * @property {boolean} production
  */
 module.exports = {
+	slackToken,
+	web,
 	dbConfig: {
 		connectionString: databaseUrl.toString(),
 		connectionTimeoutMillis: 5_000,
