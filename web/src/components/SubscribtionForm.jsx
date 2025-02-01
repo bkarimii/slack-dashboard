396ce345c7f2f2ff6a4393c410858360365e39de
karimi <behrouzkarimi1993@gmail.com>
2025-02-01 15:45:53 +0000

    component created- it returns a form to allow users subscribribtion

---
+++ web/src/components/SubscribtionForm.jsx
@@ -0,0 +1,12 @@
+function SubscribtionForm() {
+	return (
+		<>
+			<form>
+				<input type="email" placeholder="Enter your Email address."></input>
+				<button>Subscribe</button>
+			</form>
+		</>
+	);
+}
+
+export { SubscribtionForm };
