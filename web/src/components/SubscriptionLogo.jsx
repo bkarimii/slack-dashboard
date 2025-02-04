2778e0cf660475514939fa07b1b916e716415e55
karimi <behrouzkarimi1993@gmail.com>
2025-02-04 17:38:43 +0000

    component created to import the icon and reneder it in the browser

---
+++ web/src/components/SubscriptionLogo.jsx
@@ -0,0 +1,7 @@
+import subscribeIcon from "../assets/subscribeIcon.svg?url";
+
+function SubscriptionLogo() {
+	return <img src={subscribeIcon} alt="subscription-logo" />;
+}
+
+export { SubscriptionLogo };
