12cff5b2221c446f011bb2880ef156e8957f53f4
karimi <behrouzkarimi1993@gmail.com>
2025-02-06 18:40:42 +0000

    coponent imported ito home page for functionality tests

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import { SubscriptionForm } from "../components/SubscribtionForm";
+import { SubscriptionFormHTML } from "../components/SubscriptionFormHTML";
 
 function App() {
 	return (
 		<>
 			<h1>CYF Slack Dashboard</h1>
-			<SubscriptionForm />
+			<SubscriptionFormHTML />
+			{/* <SubscriptionForm /> */}
 		</>
 	);
 }
