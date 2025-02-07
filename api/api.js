8223c54d3986ad221127c4f2d467bf8019e2ead8
karimi <behrouzkarimi1993@gmail.com>
2025-02-07 16:12:07 +0000

    endpoint updated to handle incoming subscription and verify if they are a member of the relevant slack workspace with a success response

--- api/api.js
+++ api/api.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { lookupEmail } from "./functions/lookupEmails.js";
 import messageRouter from "./messages/messageRouter.js";
 
 const api = Router();
@@ -14,7 +15,17 @@ api.post("/subscribe", async (req, res) => {
 			res.status(400).json({ sucess: false, message: "bad request" });
 		}
 
-		res.status(200).json({ sucess: true });
+		const user = await lookupEmail(email);
+
+		if (user.ok) {
+			// Insert users data into DB
+
+			res.status(200).json({ sucess: true });
+		} else if (user.error === "An API error occurred: users_not_found") {
+			res.status(404).json({ sucess: false });
+		} else {
+			res.status(400).json({ sucess: false });
+		}
 	} catch (error) {
 		res.status(500).json({ message: "internal server error" });
 	}
