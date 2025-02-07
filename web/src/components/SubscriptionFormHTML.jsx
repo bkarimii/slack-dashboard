d1cb8c754576f5e01ce57108cd6eb4a7fb5648ae
karimi <behrouzkarimi1993@gmail.com>
2025-02-07 19:33:32 +0000

    styles removed

--- web/src/components/SubscriptionFormHTML.jsx
+++ web/src/components/SubscriptionFormHTML.jsx
@@ -1,25 +1,23 @@
-import { Button, Input, Space, Flex } from "antd";
+import { Button, Input, Space } from "antd";
 
 const SubscriptionFormHTML = () => {
 	return (
-		<Flex justify="center" align="center" style={{ height: "100vh" }}>
-			<div>
-				<form action="/api/subscribe" method="post">
-					<Space.Compact>
-						<Input
-							type="email"
-							id="email"
-							name="email"
-							placeholder="Enter your email"
-							required
-						/>
-						<Button type="primary" htmlType="submit">
-							Subscribe
-						</Button>
-					</Space.Compact>
-				</form>
-			</div>
-		</Flex>
+		<div>
+			<form action="/api/subscribe" method="post">
+				<Space.Compact>
+					<Input
+						type="email"
+						id="email"
+						name="email"
+						placeholder="Enter your email"
+						required
+					/>
+					<Button type="primary" htmlType="submit">
+						Subscribe
+					</Button>
+				</Space.Compact>
+			</form>
+		</div>
 	);
 };
 
