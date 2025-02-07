9b632b87d845e1de702b49e0047babae4c249b3f
karimi <behrouzkarimi1993@gmail.com>
2025-02-07 16:14:33 +0000

    email validation logic removed - was redundant due to pre built type=email property in html inputs

--- web/src/components/SubscriptionFormHTML.jsx
+++ web/src/components/SubscriptionFormHTML.jsx
@@ -1,22 +1,10 @@
-import { Button, Input, Space, Flex, message } from "antd";
+import { Button, Input, Space, Flex } from "antd";
 
 const SubscriptionFormHTML = () => {
-	// Function to validate email before submission
-	const validateEmail = (event) => {
-		const email = event.target.emailInput.value;
-
-		const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-		if (!emailPattern.test(email)) {
-			event.preventDefault();
-			message.error("Please enter a valid email address");
-		}
-	};
-
 	return (
 		<Flex justify="center" align="center" style={{ height: "100vh" }}>
 			<div>
-				<form action="/api/subscribe" method="post" onSubmit={validateEmail}>
+				<form action="/api/subscribe" method="post">
 					<Space.Compact>
 						<Input
 							type="email"
