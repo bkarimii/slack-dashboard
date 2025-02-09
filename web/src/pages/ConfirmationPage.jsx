6310a514fb566635c4475137d9a21ef16781f4fa
Ebrahim <ebrahimbeiaty@gmail.com>
2025-02-09 17:43:37 +0000

    delete extra comments

--- web/src/pages/ConfirmationPage.jsx
+++ web/src/pages/ConfirmationPage.jsx
@@ -1,54 +1,3 @@
-// import { CheckCircleOutlined } from "@ant-design/icons";
-// import { Typography, Button, Row, Col } from "antd";
-
-// import "./ConfirmationPage.css";
-
-// const { Title, Text } = Typography;
-
-// const ConfirmationPage = () => {
-// 	return (
-// 		<div className="confirmation-container">
-// 			{/* Success Icon */}
-// 			<Row justify="center">
-// 				<Col>
-// 					<CheckCircleOutlined className="success-icon" />
-// 				</Col>
-// 			</Row>
-
-// 			{/* Success Message */}
-// 			<Row justify="center">
-// 				<Col
-// 					xs={22}
-// 					sm={18}
-// 					md={12}
-// 					lg={10}
-// 					xl={8}
-// 					className="confirmation-box"
-// 				>
-// 					<Title level={3} className="success-title">
-// 						Successfully subscribed!
-// 					</Title>
-// 					<Text className="success-text">
-// 						You should receive <b>an email confirmation</b> shortly.
-// 					</Text>
-// 					<br />
-// 					<Text className="success-text">
-// 						We usually publish the newsletter on <b>every Monday</b>. Expect to
-// 						receive your first newsletter <b>this upcoming Monday</b>.
-// 					</Text>
-
-// 					{/* Close Button */}
-// 					<Button type="primary" size="large" className="close-button">
-// 						Close
-// 					</Button>
-// 				</Col>
-// 			</Row>
-// 		</div>
-// 	);
-// };
-
-// export default ConfirmationPage;
-
 import { CheckCircleFilled } from "@ant-design/icons";
 import { Typography, Button, Row, Col } from "antd";
 
@@ -73,12 +22,10 @@ const ConfirmationPage = () => {
 					textAlign: "center",
 				}}
 			>
-				{/* Success Icon */}
 				<CheckCircleFilled
 					style={{ fontSize: "60px", color: "#52c41a", marginBottom: "16px" }}
 				/>
 
-				{/* Title */}
 				<Title level={3} strong>
 					Successfully subscribed!
 				</Title>
@@ -96,7 +43,7 @@ const ConfirmationPage = () => {
 					<Button
 						size="middle"
 						style={{
-							marginTop: "14px",
+							marginTop: "15px",
 						}}
 					>
 						Close
