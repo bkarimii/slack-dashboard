ecaa266560e089ee5a801384c5d5d56ff2879a8f
jordan-choi <yyoungbest@gmail.com>
2025-02-10 23:27:49 +0000

    feat: Add global config for Antd

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,15 +1,19 @@
+import { ConfigProvider } from "antd";
 import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import About from "./pages/About.jsx";
 import Home from "./pages/Home.jsx";
+import { theme } from "./theme.js";
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/nested/about/path" element={<About />} />
-		</Routes>
+		<ConfigProvider theme={theme}>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/nested/about/path" element={<About />} />
+			</Routes>
+		</ConfigProvider>
 	);
 }
 
---
+++ web/src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+	token: {
+		fontFamily: "Roboto, sans-serif",
+	},
+	components: {
+		Typography: {
+			fontSizeHeading2: 30,
+			fontSizeHeading5: 16,
+			fontSizeBody: 14,
+			fontWeightHeading2: 500,
+			fontWeightHeading5: 400,
+			fontWeightBody: 400,
+		},
+	},
+};
