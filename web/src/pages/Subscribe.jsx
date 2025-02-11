ec280729bec5f6f03cdf31ebd18952e3b0938c9b
jordan-choi <yyoungbest@gmail.com>
2025-02-11 01:36:46 +0000

    style: Enhance layout and alignment in Subscribe component

--- web/src/pages/Subscribe.jsx
+++ web/src/pages/Subscribe.jsx
@@ -4,16 +4,22 @@ import { SubscriptionLogo } from "../components/SubscriptionLogo";
 
 function Subscribe() {
 	return (
-		<Flex vertical align="center">
+		<Flex
+			vertical
+			align="center"
+			justify="center"
+			style={{ height: "100vh" }}
+			gap={44}
+		>
 			<div style={{ width: "260px", height: "183px", overflow: "hidden" }}>
 				<SubscriptionLogo />
 			</div>
 
 			<Flex vertical gap={24} align="center">
-				<Typography.Title level={2}>
+				<Typography.Title level={2} align="center">
 					Get early access to insights on your Slack activity
 				</Typography.Title>
-				<Typography.Title level={5} type="secondary">
+				<Typography.Title level={5} type="secondary" align="center">
 					Get a weekly engagement score reflecting your activity and involvement
 					on Slack and get insights to track your contributions and stay
 					motivated.
@@ -22,7 +28,7 @@ function Subscribe() {
 
 			<Flex vertical gap={24}>
 				{/* TODO: Add subscription form */}
-				<Typography.Paragraph type="secondary">
+				<Typography.Paragraph type="secondary" align="center">
 					Subscribe now and help shape a tool that empowers you to succeed! 🚀
 				</Typography.Paragraph>
 			</Flex>
