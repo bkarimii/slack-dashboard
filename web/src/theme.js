2f56a1893606d1557da56ef86d4f9c2a0923fc6f
jordan-choi <yyoungbest@gmail.com>
2025-02-11 01:21:31 +0000

    refactor: Remove non-existent properties

--- web/src/theme.js
+++ web/src/theme.js
@@ -7,9 +7,6 @@ export const theme = {
 			fontSizeHeading2: 30,
 			fontSizeHeading5: 16,
 			fontSizeBody: 14,
-			fontWeightHeading2: 500,
-			fontWeightHeading5: 400,
-			fontWeightBody: 400,
 		},
 	},
 };
