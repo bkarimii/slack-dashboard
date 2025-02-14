d6033e7225df50744733c2b7d6868050ba9e2155
karimi <behrouzkarimi1993@gmail.com>
2025-02-14 23:55:42 +0000

    subscription and error pages imported into app.jsx component

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,7 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./pages/About.jsx";
 import { ConfirmationPage } from "./pages/ConfirmationPage.jsx";
+import { ErrorOnSubscribe } from "./pages/ErrorOnSubscribe.jsx";
 import Home from "./pages/Home.jsx";
+import { Subscription } from "./pages/Subscription.jsx";
 
 function App() {
 	return (
@@ -11,6 +13,8 @@ function App() {
 			<Route path="/" element={<Home />} />
 			<Route path="/nested/about/path" element={<About />} />
 			<Route path="/subscribe/confirmation" element={<ConfirmationPage />} />
+			<Route path="/subscribe/error" element={<ErrorOnSubscribe />} />
+			<Route path="/subscribe" element={<Subscription />} />
 		</Routes>
 	);
 }
