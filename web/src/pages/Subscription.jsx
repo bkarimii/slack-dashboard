70c1e6bffa55f7f3661611dd0705a683d2bb7137
karimi <behrouzkarimi1993@gmail.com>
2025-02-14 23:56:36 +0000

    Subscription page created

---
+++ web/src/pages/Subscription.jsx
@@ -0,0 +1,13 @@
+import { SubscriptionFormHTML } from "../components/SubscriptionFormHTML.jsx";
+import { SubscriptionLogo } from "../components/SubscriptionLogo.jsx";
+
+function Subscription() {
+	return (
+		<>
+			<SubscriptionLogo />
+			<SubscriptionFormHTML />
+		</>
+	);
+}
+
+export { Subscription };
