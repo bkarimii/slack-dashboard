f1634bd2fe1957b4c163701d6998df0eff337818
karimi <behrouzkarimi1993@gmail.com>
2025-03-17 16:08:34 +0000

    using if..else to avoid unintended issues

--- api/middlewares/zipExtractor.js
+++ api/middlewares/zipExtractor.js
@@ -3,17 +3,23 @@ import AdmZip from "adm-zip";
  * Extracts files from a ZIP buffer.
  * @param {Buffer} zipBuffer - The ZIP file buffer.
  */
-
 export const zipExtractor = (zipBuffer) => {
 	try {
 		const zip = new AdmZip(zipBuffer); // Load ZIP from memory
 		const zipEntries = zip.getEntries(); // Get list of files/folders in ZIP
 
-		const extractedFiles = zipEntries.map((entry) => ({
-			name: entry.entryName,
-			isDirectory: entry.isDirectory,
-			content: !entry.isDirectory ? entry.getData().toString() : null,
-		}));
+		const extractedFiles = zipEntries.map((entry) => {
+			let content = null;
+			if (!entry.isDirectory) {
+				content = entry.getData().toString();
+			}
+
+			return {
+				name: entry.entryName,
+				isDirectory: entry.isDirectory,
+				content,
+			};
+		});
 
 		return extractedFiles;
 	} catch (error) {
