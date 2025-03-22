1518173c703ee03787f8fb9822fe49b4fd4a2f3d
karimi <behrouzkarimi1993@gmail.com>
2025-03-22 01:21:18 +0000

    decide status updated , it returns status of a specific user based on their activity

--- api/functions/decideStatus.js
+++ api/functions/decideStatus.js
@@ -3,37 +3,38 @@ import logger from "../utils/logger";
 import { aggregateUserActivity } from "./aggregateUserActivity.js";
 import { decideScore } from "./decideScore.js";
 
-export const decideStatus = async (db, userId, startDate, endDate) => {
+export const decideStatus = async (db, userId, userActivity) => {
 	try {
 		const result = await db.query("SELECT * FROM config_table");
 
 		const configTable = result.rows[0];
 
-		const aggregatedActivity = await aggregateUserActivity(
-			db,
-			userId,
-			startDate,
-			endDate,
-		);
+		const aggregatedActivity = aggregateUserActivity(userId, userActivity);
 
+		// @todo updated version of decideScore should be replaced when it was mreged
 		if (aggregatedActivity.success) {
-			const usersScore = decideScore(
-				aggregatedActivity.usersCount.messagesCount,
-				aggregatedActivity.usersCount.reactionsCount,
+			const score = decideScore(
+				aggregatedActivity.countActivity.messagesCount,
+				aggregatedActivity.countActivity.reactionsCount,
 			);
 
-			if (usersScore < configTable.low_threshold) {
-				return "inactive";
-			} else if (usersScore < configTable.medium_threshold) {
-				return "low";
-			} else if (usersScore < configTable.high_threshold) {
-				return "medium";
+			if (score < configTable.low_threshold) {
+				return { success: true, status: "inactive" };
+			} else if (score < configTable.medium_threshold) {
+				return { success: true, status: "low" };
+			} else if (score < configTable.high_threshold) {
+				return { success: true, status: "medium" };
 			} else {
-				return "high";
+				return { success: true, status: "high" };
 			}
+		} else {
+			return {
+				success: false,
+				status: "activity has not been fetched correctly",
+			};
 		}
 	} catch (error) {
 		logger.error(error);
-		return { success: false, message: "failled to return user status" };
+		return { success: false, status: "unknown error happened" };
 	}
 };
